5776e90f4d2b719f501eefd05d7ccf9ad8efe3ae
HackerADbro <150064036+HackerADbro@users.noreply.github.com>
2023-11-10 23:20:09 +0530

    Update p_bar.py

--- p_bar.py
+++ p_bar.py
@@ -30,7 +30,7 @@ async def progress_bar(current,total,reply,start):
             tot=hrb(total)
             cur=hrb(current)
             try:
-                await reply.edit(f'`╭──⌈📤 𝙐𝙥𝙡𝙤𝙖𝙙𝙞𝙣𝙜 📤⌋──╮ \n├{progress_bar}\n├ 𝙎𝙥𝙚𝙚𝙙 : {sp} \n├ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 : {perc} \n├ 𝙇𝙤𝙖𝙙𝙚𝙙 : {cur}\n├ 𝙎𝙞𝙯𝙚 :  {tot} \n├ 𝙀𝙏𝘼 : {eta} \n╰─⌈ 𝙋𝙤𝙬𝙚𝙧𝙚𝙙 𝘽𝙮 𝘼𝙙𝙞𝙩𝙮𝙖 ⌋─╯】`')
+                await reply.edit(f'`┌ 𝙋𝙧𝙤𝙜𝙧𝙚𝙨𝙨 📈 -【 {perc} 】\n├ 𝙎𝙥𝙚𝙚𝙙 🧲 -【 {sp} 】\n└ 𝙎𝙞𝙯𝙚 📂 -【 {cur} / {tot} 】`')
                
             except FloodWait as e:
                 time.sleep(e.x)
