05015bc5634e078838744471512145aace95630a
HackerADbro <150064036+HackerADbro@users.noreply.github.com>
2023-11-10 22:29:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,9 +58,9 @@ bot = Client("bot",
              bot_token=os.environ.get("BOT_TOKEN"),
              api_id=int(os.environ.get("API_ID")),
              api_hash=os.environ.get("API_HASH"))
-auth_users = [6316847266]
+auth_users = [6693536228]
 sudo_users = auth_users
-sudo_groups = [-1001619465590]
+sudo_groups = [-4080227999]
 
 shell_usage = f"**USAGE:** Executes terminal commands directly via bot.\n\n<pre>/shell pip install requests</pre>"
 def one(user_id):
